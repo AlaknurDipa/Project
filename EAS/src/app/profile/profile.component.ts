40fc797b9101e9940bebcbc460e3d8c3be969546
Dipa Alaknur <dipaalaknur04@gmail.com>
2024-03-13 19:05:54 +0530

    profile

--- EAS/src/app/profile/profile.component.ts
+++ EAS/src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -7,5 +7,35 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-  
+    @ViewChild('addFamilyMemberRow') row!:ElementRef;
+    deleteRow(drow:any){
+      debugger
+      drow.remove();
+    }
+    addRow(){
+
+      const originalRowElement = this.row.nativeElement;
+      console.log(originalRowElement)
+      if(originalRowElement){
+        const clonedRow = originalRowElement.cloneNode(true);
+      const inputElements = clonedRow.querySelectorAll('select,input');
+      inputElements.forEach((element: any) => {
+        if (element.tagName.toLowerCase() === 'input' ) {
+          element.value = '';
+        } else if (element.tagName.toLowerCase() === 'select') {
+          element.selectedIndex = 0;
+        }
+      });
+
+      const trash = clonedRow.querySelector('.trash');
+      if (trash) {
+        trash.addEventListener('click', () => this.deleteRow(clonedRow));
+      }
+ 
+      originalRowElement.parentNode.insertBefore(clonedRow, originalRowElement.nextSibling);
+      }
+      
+ 
+    }
+
 }
